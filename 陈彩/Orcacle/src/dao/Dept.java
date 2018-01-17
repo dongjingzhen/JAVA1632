67debf9d1df181f811a71e2969cdcc61b4676a9f
LY16JAVA-chencai <34914830+LY16JAVA-chencai@users.noreply.github.com>
2018-01-17 17:56:51 +0800

    Add files via upload

---
+++ 陈彩/Orcacle/src/dao/Dept.java
@@ -0,0 +1,71 @@
+package dao;
+// default package
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Dept entity. @author MyEclipse Persistence Tools
+ */
+
+public class Dept  implements java.io.Serializable {
+
+
+    // Fields    
+
+     private int deptno;
+     private String dname;
+     private String loc;
+
+    // Constructors
+
+    /** default constructor */
+   
+    // Property accessors
+
+
+    public String getDname() {
+        return this.dname;
+    }
+    
+    @Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname 
+				+ ", loc=" + loc + "]";
+	}
+
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+
+	public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return this.loc;
+    }
+    
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+ 
+   
+
+
+
+
+
+
+
+
+}
